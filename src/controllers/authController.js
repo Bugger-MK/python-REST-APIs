9d4bb49b5ce9e5104c008c00c927884d3dff4164
Wandering Hermit <oratilemokaila91@gmail.com>
2026-04-03 20:13:37 +0200

    protect middleware created

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -23,8 +23,36 @@ exports.register = async (req, res,next) =>{
         const user = await User.create({ name, email, password });
         const token = Token(user._id);
 
-        res.status(201).json({ token });
+        res.status(201).json({success : true,data:{id:user._id,name:user.name, email:user.email}, token });
 
     } catch (error) {
         next(error);
     }   }
+
+
+
+exports.login = async (req, res,next) =>{
+    try{
+        const { email, password } = req.body; 
+        
+        if (!email || !password) {
+            return res.status(400).json({ message: "Please provide email and password" });
+        }
+       
+        const user = await User.findOne({ email });
+
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        
+        const token = signToken(user._id);
+
+        res.json({success: true,token, data:{id:user._id,name:user.name,email:user.email} });
+    }
+    catch(error){
+        next(error);
+    }}
+
+
+    exports.getMe = async (req, res,next) =>{
+       res.json({success: true, data: req.user});
