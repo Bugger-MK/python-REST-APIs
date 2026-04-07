d33299dfd22263bbd8f1d38c2cb143a7183b1db0
Wandering Hermit <oratilemokaila91@gmail.com>
2026-04-07 12:16:22 +0200

    Finished user  design

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const todoRoutes = require('./routes/todoRoutes');
 const errorHandler = require('./middleware/errorHandler');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(express.json());
 
+
+
 app.use('/api/todos', todoRoutes);
+app.use('/api/auth', authRoutes);   
 app.use(errorHandler);   // must be LAST
 
 module.exports = app;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,58 +1,52 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/users');
+const User = require('../models/User');
 
-
-const Token =(id) => {
+const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
+    expiresIn: process.env.JWT_EXPIRE,
   });
-}
+};
 
-
-exports.register = async (req, res,next) =>{    
-    try {
-        const { name, email, password } = req.body;
-
-
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-
-        const user = await User.create({ name, email, password });
-        const token = Token(user._id);
-
-        res.status(201).json({success : true,data:{id:user._id,name:user.name, email:user.email}, token });
-
-    } catch (error) {
-        next(error);
-    }   }
-
-
-
-exports.login = async (req, res,next) =>{
-    try{
-        const { email, password } = req.body; 
-        
-        if (!email || !password) {
-            return res.status(400).json({ message: "Please provide email and password" });
-        }
-       
-        const user = await User.findOne({ email });
-
-        if (!user || !(await user.comparePassword(password))) {
-            return res.status(400).json({ message: "Invalid credentials" });
-        }
-        
-        const token = signToken(user._id);
-
-        res.json({success: true,token, data:{id:user._id,name:user.name,email:user.email} });
+exports.register = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, message: 'Email already in use' });
     }
-    catch(error){
-        next(error);
-    }}
+    const user = await User.create({ name, email, password });
+    const token = signToken(user._id);
+    res.status(201).json({
+      success: true,
+      token,
+      data: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ success: false, message: 'Please provide email and password' });
+    }
+    const user = await User.findOne({ email }).select('+password');
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+    const token = signToken(user._id);
+    res.json({
+      success: true,
+      token,
+      data: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
-    exports.getMe = async (req, res,next) =>{
-       res.json({success: true, data: req.user});
+exports.getMe = async (req, res) => {
+  res.json({ success: true, data: req.user });
+};
